a86a5468feb54b9aaa94a2298fdf2a63f8f62316
zer0es <thanhdat142857@gmail.com>
2014-06-19 16:21:24 +0700

    Update main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,16 @@
 require_relative "fizzbuzz"
 
-def testef()
-  
-  puts "hiih"
-  
+puts 'Enter number to array, enter Enter key to stop'
+array = []
+input = ' '
+while input != ''
+  input = gets.chomp
+  array.push input
 end
 
-bird = Fizzbuzz.new
-
-bird.fcknumber
-
+for i in array
+  puts "fizz" if ( i % 3 == 0 && i % 5 != 0)
+  puts "buzz" if ( i % 5 == 0 && i % 3 != 0)
+  puts "fizzbuzz" if ( i % 15 == 0)
+  puts i if ( i % 5 != 0 && i % 3 != 0 )
+end
